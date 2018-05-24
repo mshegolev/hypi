55c29a94c12204ba255450f1df9311a53dbd92e0
Щеголев Михаил Владимирович <m.shchegolev@atol.ru>
2018-05-24 10:52:03 +0300

    minor fix

--- hyperion/app.py
+++ hyperion/app.py
@@ -1,31 +1,18 @@
+# -*- coding: utf-8 -*-
 #!flask/bin/python
 import json
 
 import psycopg2
-from flask import Flask, jsonify, abort, request, make_response
+from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from psycopg2.extras import RealDictCursor
 
-connect_str = "dbname='hyperion' user='qa' host='192.168.7.168' " + \
+connect_str = "dbname='hyperion' user='qa' host='10.250.0.62' " + \
               "password='qa'"
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-
-@auth.get_password
-def get_password(username):
-    if username == 'atolonline':
-        return 'atolonline'
-    return None
-
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
-
-
 @app.route('/invoices', methods=['GET'])
-# @auth.login_required
 def get_invoices():
     conn = psycopg2.connect(connect_str)
     cur = conn.cursor(cursor_factory=RealDictCursor)
@@ -33,36 +20,29 @@ def get_invoices():
         SELECT
         id,data
         FROM invoice
-        LIMIT 100
+        LIMIT 1000
     """)
     result = cur.fetchall()
     conn.close()
     return jsonify({'result': json.dumps(result, indent=2)}), 200
 
 
-#   curl -i -H "Content-Type: application/json" -X POST -d '{"data": {"uuid":"sldfkj-lasdfj-lsdfj-lsdjf", "foo": "bar"}}'
-#   http://localhost:5000/receiver
 @app.route('/receiver', methods=['POST'])
-# @auth.login_required
 def insert_invoice():
-    # if not request.json or not 'data' in request.json:
-    #     abort(400)
-    print(request.json)
     data = {
         'data': request.json
     }
     conn = psycopg2.connect(connect_str)
     cur = conn.cursor()
     cur.execute("""
-        insert into invoice ( data ) VALUES ('{}');
-    """.format(data.__format__("").replace("'", "\"")))
+         insert into invoice ( data ) VALUES (%s);
+     """, (json.dumps(data),))
     conn.commit()
     conn.close()
-    return jsonify({'result': data}), 201
+    return jsonify({'result': 'success'}), 201
 
 
 @app.route('/invoices/<string:uuid>', methods=['GET'])
-# @auth.login_required
 def get_invoice_by_uuid(uuid):
     conn = psycopg2.connect(connect_str)
     cur = conn.cursor(cursor_factory=RealDictCursor)
@@ -73,4 +53,4 @@ def get_invoice_by_uuid(uuid):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
